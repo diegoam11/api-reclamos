f12a9437b0debbd53d029263183fab9b4d0ebc8b
diegoam11 <diego.alvarez2@unmsm.com>
2023-11-03 23:35:08 -0500

    DatabaseConfigSingleton created to use the singleton design pattern

--- src/config/database.py
+++ src/config/database.py
@@ -2,8 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'postgresql://user1:123@127.0.0.1:5432/modulo_rqs'
+class DatabaseConfigSingleton:
+    _instance = None
 
-engine = create_engine(URL_DATABASE)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(DatabaseConfigSingleton, cls).__new__(cls)
+            cls._instance.engine = create_engine(URL_DATABASE)
+            cls._instance.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=cls._instance.engine)
+        return cls._instance
+
+URL_DATABASE = 'postgresql://user1:123@127.0.0.1:5432/modulo_rqs'
+database_config = DatabaseConfigSingleton()
+
 Base = declarative_base()
+
--- src/routes/reclamo_routes.py
+++ src/routes/reclamo_routes.py
@@ -1,25 +1,44 @@
 from fastapi import APIRouter, HTTPException, Depends
-from typing import Annotated
 from sqlalchemy.orm import Session
-from config.database import SessionLocal, engine
+from config.database import database_config
 from models.reclamo import Reclamo
 from repositories.reclamo_repository import ReclamoRepository
 from schemas.base import ReclamoBase
 
 router = APIRouter()
-Reclamo.metadata.create_all(bind=engine)
+Reclamo.metadata.create_all(bind=database_config.engine)
 
-def get_db():
-    db = SessionLocal()
+def get_db() -> Session:
+    db = database_config.SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
-db_dependency = Annotated[Session, Depends(get_db)]
-reclamo_repository = ReclamoRepository
+reclamo_repository = ReclamoRepository()
 
 @router.post("/reclamos/")
-async def create_reclamos(reclamo: ReclamoBase, db: db_dependency):
-    db_reclamo = reclamo_repository(db).create_reclamo(reclamo.dict())
-    return db_reclamo
+async def create_reclamos(reclamo: ReclamoBase, db: Session = Depends(get_db)):
+    try:
+        db_reclamo = reclamo_repository(db).create_reclamo(reclamo.dict())
+        return db_reclamo
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error interno del servidor")
+
+@router.get("/reclamos/")
+def get_reclamos(db: Session = Depends(get_db)):
+    try:
+        db_reclamos = reclamo_repository(db).get_reclamos()
+        return db_reclamos
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error interno del servidor")
+
+@router.get("/reclamos/{id_cliente}")
+def get_reclamo_de_cliente(id_cliente: int, db: Session = Depends(get_db)):
+    try:
+        reclamo = reclamo_repository(db).get_reclamo_by_id_cliente(id_cliente)
+        if not reclamo:
+            raise HTTPException(status_code=404, detail="Reclamo no encontrado")
+        return reclamo
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error interno del servidor")
