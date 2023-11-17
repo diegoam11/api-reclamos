799410c5bba889101a33a15629b71acf61939fc2
diegoam11 <diego.alvarez2@unmsm.com>
2023-11-17 03:55:32 -0500

    fixed solicitudes routes

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from routes import reclamo_routes, queja_routes
+from routes import reclamo_routes, queja_routes, solicitud_routes
 
 app = FastAPI()
 
 app.include_router(reclamo_routes.router)
 app.include_router(queja_routes.router)
+app.include_router(solicitud_routes.router)
 
 if __name__ == "__main__":
     import uvicorn
